8ca5a2aa412abea29dce08d19a597e172c47255b
Sushil Kumar Singh <singh.sushil721@gmail.com>
2019-08-26 17:37:52 +0530

    Update MvcConfiguration.java

--- src/main/java/com/sushil/conf/MvcConfiguration.java
+++ src/main/java/com/sushil/conf/MvcConfiguration.java
@@ -1,6 +1,5 @@
 package com.sushil.conf;
-/*package com.candidjava.conf;
-
+/*
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
